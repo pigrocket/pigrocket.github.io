cd84e905eed066c035fc670bd24ad6a8e6de0b61
pigrocket <joey.sandmeyer@gmail.com>
2019-10-12 09:20:12 -0700

    superimposing hex grid

--- test/Game.js
+++ test/Game.js
@@ -379,6 +379,53 @@ function bgcolor(str) {
     document.body.style.backgroundColor = "pink";
 }
 
+//
+
+const draw = SVG(document.body)
+const Hex = Honeycomb.extendHex({
+  size: 30,
+
+  render(draw) {
+    const { x, y } = this.toPoint()
+    const corners = this.corners()
+
+    this.draw = draw
+      .polygon(corners.map(({ x, y }) => `${x},${y}`))
+      .fill('none')
+      .stroke({ width: 1, color: '#999' })
+      .translate(x, y)
+  },
+
+  highlight() {
+    this.draw
+      // stop running animation
+      .stop(true, true)
+      .fill({ opacity: 1, color: 'aquamarine' })
+      .animate(1000)
+      .fill({ opacity: 0, color: 'none' })
+  }
+})
+const Grid = Honeycomb.defineGrid(Hex)
+const grid = Grid.rectangle({
+  width: 7,
+  height: 7,
+  // render each hex, passing the draw instance
+  onCreate(hex) {
+    hex.render(draw)
+  }
+})
+
+document.addEventListener('click', ({ offsetX, offsetY }) => {
+  const hexCoordinates = Grid.pointToHex([offsetX, offsetY])
+  const hex = grid.get(hexCoordinates)
+
+  if (hex) {
+    hex.highlight()
+  }
+})
+
+//
+
 document.onkeydown = document.onkeyup = function(e) {
   keys[e.keyCode] = e.type === "keydown";
 };
